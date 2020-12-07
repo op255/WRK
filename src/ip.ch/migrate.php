8491ca0ae7f0e15c6be2c6848e2a00498c2c0205
op255 <phyvoygue@gmail.com>
2020-12-07 14:12:06 +0700

    Provisioning fixes, migrate.php added

--- src/ip.ch/migrate.php
+++ src/ip.ch/migrate.php
@@ -29,12 +29,27 @@
         \PDO::ATTR_EMULATE_PREPARES   => false,
     ];
 
-    $dsn = "mysql:host=localhost;dbname=PostDB;charset=utf8;port=3036";
+    $dsn = "mysql:host=localhost;charset=utf8;port=3036";
     $pdo = new \PDO($dsn, 'root', 'root', $options);
-    $sql = "INSERT INTO posts (text_content) VALUES (?)";
 
-    for ($i = 0; $i < 14; ++$i) {
-        $stmt= $pdo->prepare($sql);
+    $pdo->exec("CREATE DATABASE IF NOT EXISTS `PostDB`;
+                CREATE USER 'root'@'localhost' IDENTIFIED BY 'root';
+                GRANT ALL ON `PostDB`.* TO 'root'@'localhost';
+                FLUSH PRIVILEGES;");
+    $pdo->exec("use PostDB");
+
+    $colums = "id INT AUTO_INCREMENT PRIMARY KEY, 
+                text_content TEXT NOT NULL, 
+                img_src VARCHAR( 256 ),
+                reply_to INT,
+                parent INT" ;
+
+    $pdo->exec("CREATE TABLE IF NOT EXISTS PostDB.posts ($colums)");
+
+    $sqlInsert = "INSERT INTO posts (text_content) VALUES (?)";
+
+    for ($i = 0; $i < 34; ++$i) {
+        $stmt= $pdo->prepare($sqlInsert);
         $stmt->execute([$lipsum->paragraph()]);
     }
 ?>
